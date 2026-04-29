374805b6e077f94c56eed3864b9b68601e22b929
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2026-04-29 01:12:33 -0700

    refactor: improve flake, port router config to root flake
    - Improve the flake, adding a proper description and making more things
      follow the one main Nixpkgs.
    - Ported the router's flake to the root flake, removing its own. I'm not
      entirely certain if this works, I haven't tested it yet.
    - I have not yet ported my server. I don't really care about that right
      now, so I'll leave that until later, when it'll be easier for me to do
      with more experience.

--- flake.nix
+++ flake.nix
@@ -1,11 +1,33 @@
 {
-    description = "A very basic flake";
+    description = "My NixOS configurations";
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
         nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-        nixvim.url = "github:nix-community/nixvim";
-        home-manager.url = "github:nix-community/home-manager";
+        nixvim = {
+            url = "github:nix-community/nixvim";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
+        nixpkgs-rpi.follows = "nixos-raspberrypi/nixpkgs";
+        disko-rpi = {
+            url = "github:nix-community/disko";
+            inputs.nixpkgs.follows = "nixos-raspberrypi/nixpkgs";
+        };
+    };
+
+    nixConfig = {
+        extra-substituters = [
+            "https://nixos-raspberrypi.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+            "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+        ];
     };
 
     outputs =
@@ -15,13 +37,17 @@
             nixos-wsl,
             nixvim,
             home-manager,
+
+            nixos-raspberrypi,
+            nixpkgs-rpi,
+            disko-rpi,
         }:
         {
             nixosConfigurations.wsl = inputs.nixpkgs.lib.nixosSystem {
                 system = "x86_64-linux";
                 specialArgs = { inherit inputs; };
                 modules = [
-                    ./default.nix
+                    ./hosts/loaner-laptop/default.nix
 
                     nixvim.nixosModules.nixvim
                     nixos-wsl.nixosModules.default
@@ -35,6 +61,19 @@
                             interop.register = true;
                         };
                     }
+                ];
+            };
+
+            nixosConfigurations.router = nixos-raspberrypi.lib.nixosSystem {
+                specialArgs = {
+                    inherit nixos-raspberrypi;
+                    nixpkgs = nixpkgs-rpi;
+                    disko = disko-rpi;
+                };
+                modules = [
+                    ./hosts/router/raspberry-pi.nix
+                    ./hosts/router/disko.nix
+                    ./hosts/router/configuration.nix
                 ];
             };
         };
