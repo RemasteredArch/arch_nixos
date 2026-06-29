f56aa4188088bf7330428f5294bc0e7fb2c69074
RemasteredArch <81265470+RemasteredArch@users.noreply.github.com>
2026-06-29 15:50:39 -0700

    feat(router): make NAT routing togglable
    - I don't current reside somewhere where I'm in need of my own router,
      so I've made NAT routing togglable to enable a future commit to
      disable it.

--- hosts/router/networking.nix
+++ hosts/router/networking.nix
@@ -4,89 +4,106 @@
     ...
 }:
 
+let
+    cfg = config.services.networking;
+in
 {
-    systemd.network.links = {
-        "10-on-board-ethernet" = {
-            matchConfig.PermanentMACAddress = "2c:cf:67:50:3d:bb";
-            linkConfig.Name = "ob-eth";
-        };
-        "11-add-on-ethernet" = {
-            matchConfig.PermanentMACAddress = "00:e0:4c:00:11:53";
-            linkConfig.Name = "add-eth";
-        };
-        "12-usb-ethernet" = {
-            matchConfig.PermanentMACAddress = "0c:37:96:77:8c:32";
-            linkConfig.Name = "usb-eth";
-        };
-        "15-on-board-wifi" = {
-            matchConfig.PermanentMACAddress = "ea:25:82:be:7e:b3";
-            linkConfig.Name = "ob-wlan";
+    options.services.networking = {
+        nat = lib.mkOption {
+            type = lib.types.bool;
+            description = ''
+                Whether or not this device should actually act as a NAT router.
+                Disabling this defeats the purpose of `router` being a router,
+                reducing it to just being a device on the network.
+            '';
+            default = true;
         };
     };
 
-    # Provides multicast DNS and DNS service discovery. Requires UDP port 5353 to be open.
-    services.avahi.enable = true;
+    config = {
+        systemd.network.links = {
+            "10-on-board-ethernet" = {
+                matchConfig.PermanentMACAddress = "2c:cf:67:50:3d:bb";
+                linkConfig.Name = "ob-eth";
+            };
+            "11-add-on-ethernet" = {
+                matchConfig.PermanentMACAddress = "00:e0:4c:00:11:53";
+                linkConfig.Name = "add-eth";
+            };
+            "12-usb-ethernet" = {
+                matchConfig.PermanentMACAddress = "0c:37:96:77:8c:32";
+                linkConfig.Name = "usb-eth";
+            };
+            "15-on-board-wifi" = {
+                matchConfig.PermanentMACAddress = "ea:25:82:be:7e:b3";
+                linkConfig.Name = "ob-wlan";
+            };
+        };
 
-    networking = {
-        hostName = "arch-router";
+        # Provides multicast DNS and DNS service discovery. Requires UDP port 5353 to be open.
+        services.avahi.enable = true;
 
-        # IPv6 is a myth invented by big IANA to scare you into using DHCP!
-        enableIPv6 = false;
+        networking = {
+            hostName = "arch-router";
 
-        useDHCP = true;
+            # IPv6 is a myth invented by big IANA to scare you into using DHCP!
+            enableIPv6 = false;
 
-        interfaces.ob-eth = {
             useDHCP = true;
 
-            ipv4.addresses = [
-                {
-                    address = "192.168.68.209";
-                    prefixLength = 22;
-                }
-            ];
+            interfaces.ob-eth = {
+                useDHCP = true;
 
-            wakeOnLan.enable = true;
-        };
+                ipv4.addresses = [
+                    {
+                        address = "192.168.68.209";
+                        prefixLength = 22;
+                    }
+                ];
 
-        interfaces.add-eth = {
-            useDHCP = true;
+                wakeOnLan.enable = true;
+            };
 
-            ipv4.addresses = [
-                {
-                    address = "192.168.68.208";
-                    prefixLength = 22;
-                }
-            ];
-        };
+            interfaces.add-eth = {
+                useDHCP = true;
 
-        hosts = {
-            "192.168.68.210" = [ "arch-laptop" ];
-            "192.168.68.211" = [ "arch-pc" ];
-        };
+                ipv4.addresses = [
+                    {
+                        address = "192.168.68.208";
+                        prefixLength = 22;
+                    }
+                ];
+            };
 
-        nat = {
-            enable = true;
-            externalInterface = "ob-eth";
-            internalInterfaces = [ "add-eth" ];
-        };
+            hosts = {
+                "192.168.68.210" = [ "arch-laptop" ];
+                "192.168.68.211" = [ "arch-pc" ];
+            };
 
-        # No open ports by default, only open ports on internal interfaces.
-        firewall = {
-            enable = true;
+            nat = {
+                enable = cfg.nat;
+                externalInterface = "ob-eth";
+                internalInterfaces = [ "add-eth" ];
+            };
 
-            allowedTCPPorts = [
-                22 # Used by OpenSSH.
-            ];
-            allowedUDPPorts = [
-                9 # Used for Wake on LAN.
-                5353 # Used by Avahi for mDNS/DNS-SD.
-            ];
-        };
+            # No open ports by default, only open ports on internal interfaces.
+            firewall = {
+                enable = true;
 
-        nftables = {
-            enable = true;
-            # Keep things declarative.
-            flushRuleset = true;
+                allowedTCPPorts = [
+                    22 # Used by OpenSSH.
+                ];
+                allowedUDPPorts = [
+                    9 # Used for Wake on LAN.
+                    5353 # Used by Avahi for mDNS/DNS-SD.
+                ];
+            };
+
+            nftables = {
+                enable = true;
+                # Keep things declarative.
+                flushRuleset = true;
+            };
         };
     };
 }
